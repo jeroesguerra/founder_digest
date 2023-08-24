3d26b1c78cd4296319dce5de6b6b881bce045713
jero esguerra <jeroesguerra@gmail.com>
2023-08-23 17:49:32 -0700

    Updated Dashboard panel to update status in db

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -2,9 +2,8 @@ class UserSubmissionsController < ApplicationController
     def create
         UserSubmission.create!(user_submission_params)
         redirect_to root_path, notice: 'Thanks! Your application was received.'
-
     end
-
+    
     def user_submission_params
         params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :job_role, :text, :plan_name)
     end
